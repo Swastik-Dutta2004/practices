b2391788ce40a1e33182c0339d7f93d362cd5c97
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-13 19:38:09 +0530

    4th commit

--- BrandNewDay/lib/appwrite.ts
+++ BrandNewDay/lib/appwrite.ts
@@ -1,3 +1,3 @@
 import {Client, Account, Databases} from 'react-native-appwrite'
 
-const client = new Client().setEndpoint("https://syd.cloud.appwrite.io/v1").setProject("6895fed1002019c9afa1")
+const client = new Client().setEndpoint("https://syd.cloud.appwrite.io/v1").setProject("6895fed1002019c9afa1").setPlatform("co.swastik.tracker")
