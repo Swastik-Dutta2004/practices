9f6ebe8ee56ae0e8d052c4ccd8a8fb6976748c36
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-15 12:15:45 +0530

    5th commit

--- BusBooking/src/App.jsx
+++ BusBooking/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './componenets/Navbar'
@@ -7,6 +8,16 @@ import Contact from './componenets/Contact'
 import Login from './componenets/Logoin'
 
 function App() {
+
+  const [Fromlocation, setFromlocation] = useState("")
+  const [Tolocation, setTolocation] = useState("")
+
+  const exchange = () => {
+    let temp = Fromlocation
+    setFromlocation(Tolocation)
+    setTolocation(temp)
+  }
+
   const router = createBrowserRouter([
 
     {
@@ -36,35 +47,49 @@ function App() {
       <div className="relative z-10">
         <Navbar />
         <RouterProvider router={router} />
+        <h1 className='text-white relative'>India's No. 1 online bus ticket booking site</h1>
+        <div className="bg-image">
+          <img className='w-full h-80' src="https://i.pinimg.com/1200x/7a/d7/7f/7ad77fdae977c55f86893ae07d879676.jpg" alt="" />
+        </div>
 
-        <div className="ticket mt-15 flex flex-col items-center">
-          <div className="ride bg-purple-200 p-6 rounded-xl w-80% flex gap-5 justify-center">
-            <input
-              type="text"
-              placeholder="From"
-              className="w-full border p-3 mb-4 rounded-lg focus:outline-none bg-purple-100"
-            />
+        <div className="ticket mt-15 flex flex-col items-center w-full absolute top-50">
+          <div className="ride bg-purple-200 p-6 rounded-xl w-80% flex gap-5">
+            <div className="input flex flex-col items-center justify-center gap-3">
+              <input
+                type="text"
+                placeholder="From"
+                value={Fromlocation}
+                onChange={(e) => setFromlocation(e.target.value)}
+                className="w-full border p-3 rounded-lg focus:outline-none bg-purple-100"
+              />
 
-            <img className='' src="arrow.svg" alt="" />
-            <input
-              type="text"
-              placeholder="To"
-              className="w-full border p-3 mb-4 rounded-lg focus:outline-none  bg-purple-100"
-            />
-            <input
-              type="text"
-              placeholder="Calender"
-              className="w-full border p-3 mb-4 rounded-lg focus:outline-none  bg-purple-100"
-            />
-            <input
-              type="text"
-              placeholder="Distance"
-              className="w-full border p-3 mb-4 rounded-lg focus:outline-none  bg-purple-100"
-            />
-            
+              <img className="" src="arrow.svg" alt="" onClick={exchange} />
+
+              <input
+                type="text"
+                placeholder="To"
+                value={Tolocation}
+                onChange={(e) => setTolocation(e.target.value)}
+                className="w-full border p-3 rounded-lg focus:outline-none bg-purple-100"
+              />
+            </div>
+
+            <div className="others">
+              <input
+                type="text"
+                placeholder="Calender"
+                className="w-full border p-3 mb-4 rounded-lg focus:outline-none  bg-purple-100"
+              />
+              <input
+                type="text"
+                placeholder="Distance"
+                className="w-full border p-3 mb-4 rounded-lg focus:outline-none  bg-purple-100"
+              />
+            </div>
+
           </div>
 
-          <button className="bg-amber-600 border-2 rounded-3xl w-52 h-10 font-bold mt-4 hover:bg-amber-700 transition duration-200 ">
+          <button className="bg-purple-800 text-white border-none rounded-3xl w-52 h-10 font-bold mt-4 hover:bg-purple-900 transition duration-200 ">
             Search Bus
           </button>
         </div>
