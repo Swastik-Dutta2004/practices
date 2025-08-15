cd3025baf0469c98ed13c74ef3b262dfd5064ce1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-15 09:46:25 +0530

    3rd commit

--- BusBooking/src/App.jsx
+++ BusBooking/src/App.jsx
@@ -37,13 +37,15 @@ function App() {
         <Navbar />
         <RouterProvider router={router} />
 
-        <div className="ticket mt-15 flex flex-col items-center w-full">
-          <div className="ride bg-purple-200 p-6 rounded-xl max-w-md w-full flex gap-5">
+        <div className="ticket mt-15 flex flex-col items-center">
+          <div className="ride bg-purple-200 p-6 rounded-xl w-80% flex gap-5">
             <input
               type="text"
               placeholder="From"
               className="w-full border p-3 mb-4 rounded-lg focus:outline-none bg-purple-100"
             />
+
+            <img className='' src="arrow.svg" alt="" />
             <input
               type="text"
               placeholder="To"
